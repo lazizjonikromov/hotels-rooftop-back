68f5589f70681f467f9bbfeb38d2c73adf44c74c
lazizjonikromov <lazizjonweb@gmail.com>
2024-08-17 13:19:48 +0500

    back commit, some mistake

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-  origin: 'https://hotels-rooftop.vercel.app',
+  origin: process.env.CLIENT_PATH,
   credentials: true, // enable cookies
 }));
 
